28e4eba9a4dfe6124850c15fa94bf6fbef50bedd
Linh Hoang <linhoang.ce@gmail.com>
2024-08-25 22:53:44 -0700

    commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Transformations = ({ params: id }) => {
+	return <div>Transformations</div>;
+};
+
+export default Transformations;
---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,43 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IImage extends Document {
+  title: string; 
+  transformationTypes: string; 
+  publicId: string; 
+  secureUrl: string; 
+  width?: number; 
+  height?: number; 
+  config?: Record<string, any>; 
+  transformationUrl?: URL; 
+  aspectRatio?: string; 
+  color?: string; 
+  prompt?: string; 
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  }; 
+  createdAt?: Date; 
+  updatedAt?: Date; 
+}
+
+const ImageSchema = new Schema({
+	title: { type: String, required: true },
+	transformationTypes: { type: String, required: true },
+	publicId: { type: String, required: true },
+	secureUrl: { type: String, required: true },
+	width: { type: Number },
+	height: { type: Number },
+	config: { type: Object },
+	transformationUrl: { type: URL },
+	aspectRatio: { type: String },
+	color: { type: String },
+	prompt: { type: String },
+	author: { type: Schema.Types.ObjectId, ref: "User" },
+	createdAt: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now },
+});
+
+const Image = models?.image || model("Image", ImageSchema);
+
+export default Image;
